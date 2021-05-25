a91b1fbccc192b0d46ad65b651eb4efb169a69b7
WinnieZhong1 <winnie.zhong23@sitechhs.com>
2021-05-25 19:45:31 -0400

    p

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import {useState} from "react"; 8k (gzipped:3.3K)
+
+function App(){
+    return(
+        <div className="App"> 
+            <h1>Pokemon Stats</h1>
+        </div>
+    );
+}
+
+export default App;
