b9ef740391f1317201e897862b45c06de974741d
WinnieZhong1 <winnie.zhong23@sitechhs.com>
2021-05-25 20:12:18 -0400

    e

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
-import {useState} from "react"; 8k (gzipped:3.3K)
+import {useState} from "react"; 8k (gzipped: 3.3K)
 
 function App(){
+const [pokemonName, setPokemonName]=useState("");
+
     return(
         <div className="App"> 
+        <div className ="TitleSection">
             <h1>Pokemon Stats</h1>
+        <input type="text" onChange={(event)=>{setPokemonName(event.target.value)}}></input>
+        <button>Search Pokemon</button>
+            </div>
         </div>
     );
 }
